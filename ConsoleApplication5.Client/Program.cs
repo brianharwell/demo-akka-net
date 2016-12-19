9ec42388d2c578938066c954c08df0dd3249e6f6
Brian Harwell <brianharwell@gmail.com>
2016-12-19 16:28:46 -0500

    Added remote projects

---
+++ ConsoleApplication5.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using ConsoleApplication5.Shared;
+
+namespace ConsoleApplication5.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var actorSystem = ActorSystem.Create("Demo"))
+            {
+                var actorSelection = actorSystem.ActorSelection("akka.tcp://RemoteDemo@localhost:8081/user/ConsoleActor");
+
+                actorSelection.Tell(new Message("Hello"));
+                //actorSelection.Tell("Hello");
+
+                Console.ReadLine();
+
+                //actorSystem.Stop(actorSelection.Anchor);
+
+                actorSystem.Terminate();
+            }
+        }
+    }
+}
---
+++ ConsoleApplication5.Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using ConsoleApplication5.Shared;
+
+namespace ConsoleApplication5.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var actorSystem = ActorSystem.Create("RemoteDemo"))
+            {
+                actorSystem.ActorOf(Props.Create<ConsoleActor>(), "ConsoleActor");
+
+                actorSystem.WhenTerminated.Wait();
+            }
+        }
+    }
+
+    public class ConsoleActor : TypedActor, IHandle<Message>
+    {
+        public ConsoleActor()
+        {
+            
+        }
+
+        public void Handle(Message message)
+        {
+            Console.WriteLine(message.Value);
+        }
+    }
+    
+}
